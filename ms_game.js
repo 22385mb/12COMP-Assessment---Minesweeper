11a0c9f169168650f8535cf81c4c14f015a168a9
22385mb <161382701+22385mb@users.noreply.github.com>
2024-03-12 21:20:57 +1300

    Update ms_game.js
    Tile grid works(so far)

--- ms_game.js
+++ ms_game.js
@@ -9,7 +9,9 @@ console.log("%c Mine Sweeper Type Game", "color: blue;");
 /******************************************************/
 const SCREENWIDTH = 600;
 const SCREENHEIGHT = 700;
-const BLOCKSIZE = 37.5;
+const TILESIZE = 37.5;
+// Total number of tiles in the 16 by 16 grid
+const TILENUM = 256; 
 
 /******************************************************/
 // setup()
@@ -17,6 +19,8 @@ const BLOCKSIZE = 37.5;
 function setup() {
     console.log("setup: ");
     cnv = new Canvas(SCREENWIDTH, SCREENHEIGHT);
+    //Creates the tile sprites
+    createSprites();
 }
 
 /******************************************************/
@@ -24,14 +28,38 @@ function setup() {
 /******************************************************/
 function draw() {
     background("lightgrey");
-    createSprites();
 }
 
 /******************************************************/
 // FUNCTIONS
 /******************************************************/
 function createSprites() {
-    block = new Sprite(0 + BLOCKSIZE/2, SCREENHEIGHT - BLOCKSIZE/2, BLOCKSIZE, BLOCKSIZE, 'n');
-    block.color = "lightgreen";
+    //Variables which denote where the sprite should be placed
+    var tileXPos = 0 + TILESIZE/2;
+    var tileYPos = SCREENHEIGHT - TILESIZE/2;
+    // Logs how many tiles are in the current row
+    var rowCounter = 0;
+    
+    //Loop to spawn all tiles
+    for(var i=0; i < TILENUM; i++) {
+        /*If the row is not filled then the next spawned tile will
+        spawn to the right of the previous tile. If the row is filled
+        then the next tile will move up and go back to the start 
+        posiiton this continues until all 16 rows 
+        are filled*/
+        if(rowCounter > 0 && rowCounter < 16) {
+            tileXPos += TILESIZE;
+        } else if(rowCounter == 16) {
+            // Start new row so rowCounter set back to 0
+            rowCounter = 0;
+            //Y spawn move up. X spawn reverts to original
+            tileYPos -= TILESIZE;
+            tileXPos = 0 + TILESIZE/2;
+        }
+        // Create a tile sprite
+        tile = new Sprite(tileXPos, tileYPos, TILESIZE, TILESIZE, 'n');
+        tile.color = "lightgreen";
+        // Log that another tile has been added to the row
+        rowCounter += 1;
+    }
 }
-
